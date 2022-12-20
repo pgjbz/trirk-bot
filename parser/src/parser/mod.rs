e02296713508b5dc581e7708cab858f367abfd96
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2022-12-19 21:04:03 -0300

    feat: parse tags

--- parser/src/parser/mod.rs
+++ parser/src/parser/mod.rs
@@ -43,19 +43,39 @@ impl TrirkParser {
                 }
                 "emotes" => {
                     let emotes: Vec<Emote> = self.parse_emotes(value);
+                    tags.emotes(emotes);
                 }
-                /*
-
-                id=c285c9ed-8b1b-4702-ae1c-c64d76cc74ef;
-                mod=0;
-                room-id=81046256;
-                subscriber=0;
-                turbo=0;
-                tmi-sent-ts=1550868292494;
-                user-id=81046256;
-                user-type=staff
-                flags=0-7:A.6/P.6,25-36:A.1/I.2;
-                */
+                "id" => {
+                    tags.id(value);
+                }
+                "mod" => {
+                    tags.r#mod(value == "1");
+                }
+                "room-id" => {
+                    tags.room_id(value);
+                }
+                "subscriber" => {
+                    tags.subscriber(value == "1");
+                }
+                "turbo" => {
+                    tags.turbo(value == "0");
+                }
+                "tmi-sent-ts" => {
+                    let Ok(value) = value.parse::<usize>() else {continue;};
+                    tags.tmi_sent_ts(value);
+                }
+                "user-id" => {
+                    tags.user_id(value);
+                }
+                "user-type" => {
+                    tags.user_type(value);
+                }
+                "vip" => {
+                    tags.vip(value == "1");
+                }
+                "reply-parent-msg-id" => {
+                    tags.reply_parent_msg_id(value);
+                }
                 _ => continue,
             }
         }
